ae9c9b2d66919f2c643fe727c8d4cdff8b1c375b
Andrey Klyuyko <kairgrw@mail.ru>
2015-08-27 21:36:52 +0300

    Create README.m

---
+++ README.m
@@ -0,0 +1,4 @@
+# SCINOLibrary
+This project have been created during practics in SCINO Laboratory from August, 3 till August, 30 in 2015.
+As a result of this project we have a client-server application that realizes Book Exchange between people in the world.
+This project uses ASP.NET MVC 5 and Entity Framework 6 Code First Technology, Views are writted with support Razor engine.
